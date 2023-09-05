66d95b58926f24dec92c8c920bdb264da04f5244
Pyae Sone <pyaesone.perfect2014@gmail.com>
2023-09-05 12:02:56 +0800

    Make the initial Components

--- personal-website/src/App.jsx
+++ personal-website/src/App.jsx
@@ -1,35 +1,105 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Hero = () => {
+  return(
+    <div>
+      <HeroImg />
+      <AboutMe />
+    </div>
+  )
+}
 
+const HeroImg = () => {
   return (
+    <img/>
+  )
+}
+
+const AboutMe = () => {
+  return(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>About Me</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis optio vitae, commodi architecto reprehenderit explicabo ipsa mollitia sapiente expedita, atque voluptates deleniti reiciendis dolores, provident delectus autem accusamus asperiores quidem!</p>
+    <div></div>
     </>
   )
+}
+
+const Projects = () => {
+  return(
+    <>
+    <h2>MY Works</h2>
+    <ProjectCard />
+    <ProjectCard />
+    <ProjectCard />
+    <ProjectCard />
+    <ProjectCard />
+    <ProjectCard />
+    </>
+  )
+}
+
+const ProjectCard = () => {
+  return(
+    <>
+    <ProjectImg />
+    <ProjectInfo />
+    </>
+  )
+}
+
+const ProjectImg = () => {
+  return(
+    <>
+    <img></img>
+    </>
+  )
+}
+
+const ProjectInfo = () => {
+  return(
+    <>
+    <h3>Project Name</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores quod accusantium nemo harum tenetur perferendis, officiis, voluptatem eveniet illum eos excepturi, nesciunt aliquid fugit esse! Totam quod asperiores iste esse?</p>
+    </>
+  )
+}
+
+const Contact = () => {
+  return(
+    <>
+    <ContactInfo />
+    <ContactImg />
+    </>
+  )
+}
+
+const ContactInfo = () => {
+  return(
+    <>
+    <h2>Contact Me</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam sequi iure omnis porro excepturi totam nostrum, ad numquam illo provident a, vel voluptatem nam fugit tempore eius labore aliquid quidem?</p>
+    </>
+  )
+}
+
+const ContactImg = () => {
+  return(
+    <>
+    <img></img>
+    </>
+  )
+}
+
+function App() {
+return(
+  <>
+  <Hero />
+  <Projects />
+  <Contact />
+  </>
+)
 }
 
 export default App
