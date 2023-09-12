f7273c1ee01af694754c15bab39399fca4955c93
Pyae Sone <pyaesone.perfect2014@gmail.com>
2023-09-12 19:21:41 +0800

    Make a grid layout for projects component

--- personal-website/src/components/Projects.jsx
+++ personal-website/src/components/Projects.jsx
@@ -1,40 +1,42 @@
+import './Projects.css'
+
 const Projects = () => {
     return(
-      <>
+      <section>
       <h2>MY Works</h2>
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      </>
+      <div className='projects'>
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+      </div>
+      </section>
     )
   }
   
   const ProjectCard = () => {
     return(
-      <>
+      <div className='project-card'>
       <ProjectImg />
       <ProjectInfo />
-      </>
+      </div>
     )
   }
   
   const ProjectImg = () => {
     return(
-      <>
       <img></img>
-      </>
     )
   }
   
   const ProjectInfo = () => {
     return(
-      <>
+      <div className='project-info'>
       <h3>Project Name</h3>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores quod accusantium nemo harum tenetur perferendis, officiis, voluptatem eveniet illum eos excepturi, nesciunt aliquid fugit esse! Totam quod asperiores iste esse?</p>
-      </>
+      </div>
     )
   }
 
