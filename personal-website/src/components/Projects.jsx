703bbadc7b5c3b9317688ba1c6ac538652e4e1ce
Pyae Sone <pyaesone.perfect2014@gmail.com>
2023-09-13 18:36:09 +0800

    Add imagses and hover effects to the projects

--- personal-website/src/components/Projects.jsx
+++ personal-website/src/components/Projects.jsx
@@ -1,14 +1,22 @@
 import './Projects.css'
+import tictactoe from '../assets/Web capture_13-2-2023_23127_pyaesonep.github.io.jpeg'
+import bookStore from '../assets/Web capture_10-2-2023_125022_pyaesonep.github.io.jpeg'
+import inputForm from '../assets/Web capture_10-2-2023_124843_pyaesonep.github.io.jpeg'
+import etchaSketch from '../assets/Web capture_10-2-2023_12534_pyaesonep.github.io.jpeg'
 
 const Projects = () => {
+  const ticTacToeImg = tictactoe;
+  const bookStoreImg = bookStore;
+  const form = inputForm;
+  const etchASketch = etchaSketch;
     return(
       <section>
       <h2>MY Works</h2>
       <div className='projects'>
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
+          <ProjectCard imgSrc = {ticTacToeImg}/>
+          <ProjectCard imgSrc = {bookStoreImg}/>
+          <ProjectCard imgSrc = {form}/>
+          <ProjectCard imgSrc = {etchASketch}/>
           <ProjectCard />
           <ProjectCard />
       </div>
@@ -16,18 +24,19 @@ const Projects = () => {
     )
   }
   
-  const ProjectCard = () => {
+  const ProjectCard = (props) => {
+    const src = props.imgSrc;
     return(
       <div className='project-card'>
-      <ProjectImg />
+      <ProjectImg src = {src} />
       <ProjectInfo />
       </div>
     )
   }
   
-  const ProjectImg = () => {
+  const ProjectImg = (props) => {
     return(
-      <img></img>
+      <img src = {props.src}></img>
     )
   }
   
